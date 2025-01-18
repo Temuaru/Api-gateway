d6e252ef99ebed9b0f920799f871f8c24a2e1bb9
Temuaru <88447165+Temuaru@users.noreply.github.com>
2025-01-18 13:40:26 +0400

    Add files via upload

---
+++ Microservices/api-gateway/demo/src/test/java/api_gateway/example/demo/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package api_gateway.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
